d12b0ff6572abdc97db33a06ffade13d3865d0a4
alexandermcmillan <mcmillancontact@protonmail.com>
2023-10-13 18:44:25 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,48 +1,36 @@
 // Code your solution in this file!
 
+const hq = 42
+
+function distanceFromHqInBlocks(pickUp) {
+  //returns the number of blocks given a value
+
+  return hq > pickUp ? hq - pickUp : pickUp - hq
+};
+
+//distanceFromHqInFeet: Returns the number of feet from Scuber's headquarters to the pickup location. Use your distanceFromHqInBlocks function to help return the correct value here. Try something like this:
+function distanceFromHqInFeet(pickUp) {
+  return distanceFromHqInBlocks(pickUp) * 264
+};
 
 
-function distanceFromHqInBlocks (block) {
-    if (block<42){
-      return(42-block);
-    }
-    else {
+function distanceTravelledInFeet(start, destination) {
+  //returns the number of feet traveled
+  let feetDistance = start > destination ? start - destination :
+    destination - start;
+  return feetDistance * 264
+};
 
-    return(block-42);
-  }
-  }
+// calculatesFarePrice: Given the same starting and ending block as the previous test (hint hint), return the fare for the customer. The first four hundred feet are free. For a distance between 400 and 2000 feet, the price is 2 cents per foot (not including 400, which are free!). Then Scuber charges a flat fare of $25 for a distance over 2000 feet and under 2500 feet. Finally, Scuber does not allow any rides over 2500 feet — the function returns 'cannot travel that far' if a ride over 2500 feet is requested.
 
+function calculatesFarePrice(start, destination) {
+  //returns the fare for the customer
+  let distance = distanceTravelledInFeet(start, destination);
 
+  if (distance <= 400) return 0;
+  if (distance > 400 && distance <= 2000)
+    return (distance - 400) * .02;
+  if (distance > 2000 && distance <= 2500) return 25;
+  if (distance > 2500) return `cannot travel that far`;
 
-
-function distanceFromHqInFeet(block) {
-
-    return distanceFromHqInBlocks(block)* 264;
-}
-
-
-function distanceTravelledInFeet(a,b){
-    if (b>a){
-      return((b-a)*264);
-    }
-    else
-      return((a-b)*264);
-  }
-
-
-
-  function calculatesFarePrice(start,destination){
-    let distance = distanceTravelledInFeet(start,destination);
-    if (distance<= 400){
-      return 0;
-    }
-    if (distance>=400 && distance<=2000){
-      return (distance-400)* 0.02;
-    }
-    else if(distance>=2000&& distance<=2500){
-      return 25;
-    }
-    else if (distance > 2500){
-      return "cannot travel that far";
-    }
-  }
+};
